255b6ddb4135666689fae49435e2af8a9f05a565
AnayaChoudhari <118365853+AnayaChoudhari@users.noreply.github.com>
2023-05-01 19:34:07 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+      <a href="tel:1234567890"><i class="fas fa-phone fa-beat"></i><span>123456789</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone fa-beat"></i><span>1112223333</span></a>
+         <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope fa-beat"></i><span>shaikhanass@gmail.com</span></a>
+         <a href="#"><i class="fa-solid fa-location-crosshairs fa-beat-fade"></i><span>mumbai, india - 400104</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fa-brands fa-facebook fa-beat"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter fa-beat"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin fa-beat"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram fa-beat"></i></a>
+
+      </div>
+
+   </section>
+
+  
+
+</footer>
+
+<!-- footer section ends -->
